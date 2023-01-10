b0e755d46e333d94fb486ee511d992324d7a74b3
toto9602 <toto9602@naver.com>
2023-01-10 20:23:32 +0900

    [feat] : DB모듈, ConfigModule 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './common/database/database.module';
+import { getEnvPath } from './common/helper/env.helper';
+
+const envFilePath = getEnvPath(`${__dirname}/common/envs`);
 
 @Module({
-  imports: [],
+  imports: [
+    DatabaseModule,
+    ConfigModule.forRoot({
+      envFilePath,
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
