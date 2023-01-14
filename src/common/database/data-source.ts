a0f7e87ea00a6e0bd40317565ba0ba6b8490c66b
toto9602 <toto9602@naver.com>
2023-01-14 14:25:58 +0900

    Revert "[feat] : 테스트 목적 DB 세팅"
    This reverts commit a99f81fbb21bd249a262541823c882625ce97b69.

--- src/common/database/data-source.ts
+++
@@ -1,8 +0,0 @@
-import { DataSource } from 'typeorm';
-
-export const AppDataSource = new DataSource({
-  type: 'sqlite',
-  database: 'test.sqlite3',
-  synchronize: true,
-  entities: ['src/**/entities/*.entity.ts'],
-});
--- src/common/di-tokens/db.di-token.ts
+++
@@ -1,1 +0,0 @@
-export const DATA_SOURCE_TOKEN = Symbol('DATA_SOURCE_TOKEN');
