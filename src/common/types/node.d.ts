ed860087fe156a0fa6e96fd19a6e4e8691fbb48b
toto9602 <toto9602@naver.com>
2023-02-06 23:56:14 +0900

    [#7][feat] dataSource 수정 및 env 업데이트

--- src/common/types/node.d.ts
+++ src/common/types/node.d.ts
@@ -1,6 +1,12 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
+      DB_HOST: string;
+      DB_PORT: string;
+      DB_USER: string;
+      DB_PASS: string;
+      DB_NAME: string;
+      MYSQL_ROOT_PASSWORD: string;
       NODE_ENV: string;
       DB_NAME: string;
     }
